3df6799c362f454ab07456a7e5c1cab387985af1
crynobone <crynobone@gmail.com>
2013-04-12 10:26:05 +0800

    Add config and default Model
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Memory/Model.php
@@ -0,0 +1,11 @@
+<?php namespace Orchestra\Memory;
+
+class Model extends \Illuminate\Database\Eloquent\Model {
+	
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'orchestra_options';
+}
---
+++ src/config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Default Table for Memory with Fluent Driver
+	|--------------------------------------------------------------------------
+	|
+	| Specify the default table name to be used when using 
+	| `Orchestra\Memory::make('fluent');`.
+	|
+	*/
+
+	'default_table' => 'orchestra_options',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Model for Memory with Eloquent Driver
+	|--------------------------------------------------------------------------
+	|
+	| Specify the default model name to be used when using 
+	| `Orchestra\Memory::make('eloquent');`.
+	|
+	*/
+
+	'default_model' => 'Orchestra\Memory\Model',
+);
