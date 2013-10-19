6d04e52a3103588c8cb2bdd13dc82b925c546c50
crynobone <crynobone@gmail.com>
2013-10-20 00:23:28 +0800

    Implement PSR-2 code standard.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Memory/MemoryServiceProvider.php
+++ src/Orchestra/Memory/MemoryServiceProvider.php
@@ -3,53 +3,50 @@
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 
-class MemoryServiceProvider extends ServiceProvider {
+class MemoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['orchestra.memory'] = $this->app->share(function ($app) {
+            return new MemoryManager($app);
+        });
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['orchestra.memory'] = $this->app->share(function($app)
-		{
-			return new MemoryManager($app);
-		});
+        $this->app->booting(function () {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('Orchestra\Memory', 'Orchestra\Support\Facades\Memory');
+        });
+    }
 
-		$this->app->booting(function()
-		{
-			$loader = AliasLoader::getInstance();
-			$loader->alias('Orchestra\Memory', 'Orchestra\Support\Facades\Memory');
-		});
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $path = realpath(__DIR__.'/../../');
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$path = realpath(__DIR__.'/../../');
+        $this->package('orchestra/memory', 'orchestra/memory', $path);
 
-		$this->package('orchestra/memory', 'orchestra/memory', $path);
+        $this->registerMemoryEvent();
+    }
 
-		$this->registerMemoryEvent();
-	}
+    /**
+     * Register memory events during booting.
+     *
+     * @return void
+     */
+    protected function registerMemoryEvent()
+    {
+        $app = $this->app;
 
-	/**
-	 * Register memory events during booting.
-	 *
-	 * @return void
-	 */
-	protected function registerMemoryEvent()
-	{
-		$app = $this->app;
-
-		$app->after(function() use ($app)
-		{
-			$app['orchestra.memory']->finish();
-		});
-	}
+        $app->after(function () use ($app) {
+            $app['orchestra.memory']->finish();
+        });
+    }
 }
