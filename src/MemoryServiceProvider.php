f67f59965cdef743e15963c8322a73fc1def9f1d
crynobone <crynobone@gmail.com>
2015-03-18 18:23:20 +0800

    Implements latest orchestra/support.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/MemoryServiceProvider.php
+++ src/MemoryServiceProvider.php
@@ -34,7 +34,9 @@ class MemoryServiceProvider extends ServiceProvider
 
         $this->addConfigComponent('orchestra/memory', 'orchestra/memory', $path.'/config');
 
-        $this->bootUnderLaravel($path);
+        if (! $this->hasPackageRepository()) {
+            $this->bootUnderLaravel($path);
+        }
 
         $this->bootMemoryEvent();
     }
@@ -62,13 +64,11 @@ class MemoryServiceProvider extends ServiceProvider
      */
     protected function bootUnderLaravel($path)
     {
-        if (!$this->app['config'] instanceof PackageRepository) {
-            $this->mergeConfigFrom("{$path}/config/config.php", 'orchestra.memory');
+        $this->mergeConfigFrom("{$path}/config/config.php", 'orchestra.memory');
 
-            $this->publishes([
-                "{$path}/config/config.php"   => config_path('orchestra/memory.php'),
-                "{$path}/database/migrations" => base_path('/database/migrations'),
-            ]);
-        }
+        $this->publishes([
+            "{$path}/config/config.php"   => config_path('orchestra/memory.php'),
+            "{$path}/database/migrations" => base_path('/database/migrations'),
+        ]);
     }
 }
